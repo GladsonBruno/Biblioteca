a97f44a6aa0bbcdf85a33381b9ff3b28cb5e0ec2
Gladson Santos <gladsonbruno16@gmail.com>
2017-12-23 15:52:48 -0200

    Implementando INotifyPropertyChanged

--- Biblioteca/Models/Livro.cs
+++ Biblioteca/Models/Livro.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace Biblioteca.Models
 {
-    public class Livro
+    public class Livro: INotifyPropertyChanged
     {
+
+             /*
+             - ISBN (chave primária)
+             - Título
+             - Autores (um ou mais)
+             - Editora
+             - Data de publicação
+             - Preço (em reais)
+             - Críticas de leitores (uma ou mais)
+             */
+
+
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string prop)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
     }
 }
