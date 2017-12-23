3bd00e992a521ebeb57f67fa973792432302b380
Gladson Santos <gladsonbruno16@gmail.com>
2017-12-23 16:34:41 -0200

    Criando Projeto cliente

--- Biblioteca/Models/Livro.cs
+++ Biblioteca/Models/Livro.cs
@@ -6,35 +6,124 @@ using System.Web;
 
 namespace Biblioteca.Models
 {
-    public class Livro: INotifyPropertyChanged
+    public class Livro : NotifyBase
     {
 
-             /*
-             - ISBN (chave primária)
-             - Título
-             - Autores (um ou mais)
-             - Editora
-             - Data de publicação
-             - Preço (em reais)
-             - Críticas de leitores (uma ou mais)
-             */
-
-
-
-
-
-
-
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void RaisePropertyChanged(string prop)
+        /*
+        - ISBN (chave primária)
+        - Título
+        - Autores (um ou mais)
+        - Editora
+        - Data de publicação
+        - Preço (em reais)
+        - Críticas de leitores (uma ou mais)
+        */
+        private string _ISBN;
+        public string ISBN
         {
-            if(PropertyChanged != null)
+            get { return _ISBN; }
+
+            set
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                if(_ISBN != value)
+                {
+                    _ISBN = value;
+                    RaisePropertyChanged();
+                }
             }
         }
+
+        private string _titulo;
+        public string Titulo
+        {
+            get { return _titulo; }
+
+            set
+            {
+                if(_titulo != value)
+                {
+                    _titulo = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string _autores;
+        public string Autores
+        {
+            get { return _autores; }
+
+            set
+            {
+                if(_autores != value)
+                {
+                    _autores = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        private string _editora;
+        public string Editora
+        {
+            get { return _editora; }
+
+            set
+            {
+                if(_editora != value)
+                {
+                    _editora = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string _data_publicacao;
+        public string Data_Publicacao
+        {
+            get { return _data_publicacao; }
+
+            set
+            {
+                if(_data_publicacao != value)
+                {
+                    _data_publicacao = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private double _preco;
+        public double Preco
+        {
+            get { return _preco; }
+
+            set
+            {
+                if(_preco != value)
+                {
+                    _preco = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        private string _critica_de_leitores;
+        public string Critica_de_Leitores
+        {
+            get { return _critica_de_leitores; }
+
+            set
+            {
+                if(_critica_de_leitores != value)
+                {
+                    _critica_de_leitores = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
     }
 }
---
+++ Biblioteca/Models/NotifyBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace Biblioteca.Models
+{
+    public abstract class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
